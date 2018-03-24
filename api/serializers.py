97d054ee121810801b9c71783711488a3dae332d
Jefwillems <jefwillems100@gmail.com>
2018-03-25 00:36:57 +0100

    init

---
+++ api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from api.models import Profile
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(min_length=8, trim_whitespace=True, write_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ('id',
+                  'url',
+                  'username',
+                  'email',
+                  'password',
+                  'groups',
+                  'birthday',
+                  'bio',
+                  'followers')
+
+    def create(self, validated_data):
+        user = Profile.objects.create_user(validated_data['username'], validated_data['email'],
+                                           password=validated_data['password'])
+        return user
+
+    def update(self, instance, validated_data):
+        # TODO
+        # user = Profile.objects.filter(id=instance.id).update()
+
+        user = super().update(instance, validated_data)
+        user.set_password(validated_data['password'])
+        return user
---
+++ api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from api.models import Profile
+from api.serializers import ProfileSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
