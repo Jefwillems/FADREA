fff9a59bafc63a40308151a0c237eb1bba1e4da5
Jefwillems <jefwillems100@gmail.com>
2018-04-29 19:05:49 +0200

    fixed mobile menu

--- main/static/main/js/global.js
+++ main/static/main/js/global.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("mobile-nav").innerHTML = document.getElementById("desktop-nav").innerHTML;
+    let links = Array.prototype.slice.call(document.querySelectorAll(".nav-row-container a"));
+    links = links.map(e => {
+        let li = document.createElement('li');
+        li.appendChild(e);
+        return li;
+    });
+    let mob = document.getElementById("mobile-nav");
+    links.forEach(e => {
+        mob.appendChild(e);
+    });
     const elem = document.querySelector('.sidenav');
     const instance = M.Sidenav.init(elem, {});
 });
